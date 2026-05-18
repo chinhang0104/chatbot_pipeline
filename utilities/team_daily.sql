0464befde019316615c01844013f3e945d1c04ca
chinhang0104 <57591312+chinhang0104@users.noreply.github.com>
2026-05-18 23:10:59 +0800

    team_daily sql function

---
+++ utilities/team_daily.sql
@@ -0,0 +1,13 @@
+WITH team_daily AS (
+    SELECT 
+        team,
+        checkpoint_date,
+        SUM(checkpoint_count) as team_checkpoint_count,
+        SUM(thread_count) as team_thread_count,
+        COUNT(DISTINCT user_id) as team_user_count,
+        MAX(checkpoint_date) as latest_date
+    FROM main.chat_history.gold_user_team_metrics
+    GROUP BY team, checkpoint_date
+)
+
+SELECT * FROM team_daily
